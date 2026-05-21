f64a8b32e2e6e8d785111f7d890260f2c9bfbf8f
PurpeI <zaporozankosta@gmail.com>
2026-05-22 04:08:45 +0700

    asd

--- Assets/Scripts/WorldGeneration.cs
+++ Assets/Scripts/WorldGeneration.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -9,6 +9,7 @@ public class WorldGeneration : MonoBehaviour
     public int worldX, worldY;
     public int minRoom, maxRoom;
     public int countCenterPointRoom;
+    private int actualRoomCount;
 
     [HideInInspector]
     public int netWeightRoom;
@@ -43,6 +44,10 @@ public class WorldGeneration : MonoBehaviour
     public Transform parentForDecorates;
 
     private List<RuntimeEnemy> runtimeEnemies = new List<RuntimeEnemy>();
+    private List<int>[] roomGraph;
+
+   
+    private int[] roomSizes;
 
     [Serializable]
     public class Enemy
@@ -78,15 +83,20 @@ public class WorldGeneration : MonoBehaviour
         SetEnemiesCount();
         CreateWorld();
 
-        GetComponent<MozgGeneration>().player = CreateObjectsOnCoord(player, centers[0, 0], centers[0, 1]);
+        
+        GetComponent<MozgGeneration>().player =
+            CreateObjectsOnCoord(player, centers[0, 0], centers[0, 1]);
 
-        int portalsToSpawn = Mathf.Min(7, countCenterPointRoom - 1);
+        int portalsToSpawn = Mathf.Min(actualRoomCount, actualRoomCount - 1);
         for (int i = 0; i < portalsToSpawn; i++)
         {
             CreateObjectsOnCoord(portalOnStart, centers[i, 0], centers[i, 1]);
         }
 
-        GetComponent<MozgGeneration>().portalOld = CreateObjectsOnCoord(portal, centers[countCenterPointRoom - 1, 0], centers[countCenterPointRoom - 1, 1]).transform;
+        GetComponent<MozgGeneration>().portalOld =
+            CreateObjectsOnCoord(portal,
+                centers[actualRoomCount - 1, 0],
+                centers[actualRoomCount - 1, 1]).transform;
     }
 
     void InitializeRuntimeEnemies()
@@ -115,102 +125,24 @@ public class WorldGeneration : MonoBehaviour
 
     void CreateWorld()
     {
-        world = new int[worldX + 200, worldY + 200];
         int offsetX = 100;
         int offsetY = 100;
 
-        for (int x = 0; x < worldX + 200; x++)
-        {
-            for (int y = 0; y < worldY + 200; y++)
-            {
+        world = new int[worldX + 200, worldY + 200];
+
+        for (int x = 0; x < world.GetLength(0); x++)
+            for (int y = 0; y < world.GetLength(1); y++)
                 world[x, y] = -1;
-            }
-        }
 
-        centers = new int[countCenterPointRoom, 2];
-        HashSet<string> occupiedCenters = new HashSet<string>();
-        int mapCenterX = worldX / 2;
-        int mapCenterY = worldY / 2;
-        int scatterRadius = 65;
-        centers[0, 0] = mapCenterX;
-        centers[0, 1] = mapCenterY;
-        int startRoomRange = 3;
-        for (int _x = -startRoomRange; _x <= startRoomRange; _x++)
-        {
-            for (int _y = -startRoomRange; _y <= startRoomRange; _y++)
-            {
-                int targetX = mapCenterX + offsetX + _x;
-                int targetY = mapCenterY + offsetY + _y;
-                if (targetX >= 0 && targetX < world.GetLength(0) && targetY >= 0 && targetY < world.GetLength(1))
-                {
-                    world[targetX, targetY] = 0;
-                }
-            }
-        }
-        occupiedCenters.Add(mapCenterX + "_" + mapCenterY);
+        int targetRoomCount = countCenterPointRoom;
 
-        int spawnedCenters = 1;
-        int safetyTicks = 0;
+        roomSizes = new int[targetRoomCount];
+        centers = new int[targetRoomCount, 2];
 
-        while (spawnedCenters < countCenterPointRoom && safetyTicks < 5000)
-        {
-            safetyTicks++;
-            int _x = UnityEngine.Random.Range(mapCenterX - scatterRadius, mapCenterX + scatterRadius);
-            int _y = UnityEngine.Random.Range(mapCenterY - scatterRadius, mapCenterY + scatterRadius);
-            string key = _x + "_" + _y;
+        actualRoomCount = PlaceRooms(offsetX, offsetY, targetRoomCount);
 
-            if (!occupiedCenters.Contains(key))
-            {
-                occupiedCenters.Add(key);
-                world[_x + offsetX, _y + offsetY] = 0;
-                centers[spawnedCenters, 0] = _x;
-                centers[spawnedCenters, 1] = _y;
-                spawnedCenters++;
-            }
-        }
-        for (int i = 1; i < countCenterPointRoom; i++)
-        {
-            int centerX = centers[i, 0] + offsetX;
-            int centerY = centers[i, 1] + offsetY;
-            int roomRange = UnityEngine.Random.Range(minRoom, maxRoom);
-            if (roomRange < 1) roomRange = 1;
-
-            int minX = 0; int maxX = 0;
-            int minY = 0; int maxY = 0;
-
-            for (int _x = -roomRange; _x <= roomRange; _x++)
-            {
-                for (int _y = -roomRange; _y <= roomRange; _y++)
-                {
-                    int targetX = centerX + _x;
-                    int targetY = centerY + _y;
-
-                    if (targetX >= 0 && targetX < world.GetLength(0) && targetY >= 0 && targetY < world.GetLength(1))
-                    {
-                        world[targetX, targetY] = 0;
-
-                        if (_x < minX) minX = _x;
-                        if (_x > maxX) maxX = _x;
-                        if (_y < minY) minY = _y;
-                        if (_y > maxY) maxY = _y;
-                    }
-                }
-            }
-
-            CreateEnemies(centers[i, 0], centers[i, 1], minX, maxX, minY, maxY);
-            SetRoomsDecos(centerX, centerY, minX, maxX, minY, maxY);
-        }
-
-        for (int i = 0; i < countCenterPointRoom - 1; i++)
-        {
-            int startX = centers[i, 0] + offsetX;
-            int startY = centers[i, 1] + offsetY;
-            int endX = centers[i + 1, 0] + offsetX;
-            int endY = centers[i + 1, 1] + offsetY;
-
-            DigTunnelX(startX, endX, startY);
-            DigTunnelY(startY, endY, endX);
-        }
+        BuildRoomGraph(actualRoomCount);
+        DigTunnelsByGraph(offsetX, offsetY, actualRoomCount);
 
         tilemapWall.ClearAllTiles();
 
@@ -223,25 +155,117 @@ public class WorldGeneration : MonoBehaviour
                 Vector3Int tilePos = new Vector3Int(logicX, logicY, 0);
 
                 if (world[x, y] == -1)
-                {
                     tilemapWall.SetTile(tilePos, wall);
-                }
-                else if (world[x, y] >= 10)
-                {
-                    int decoIndex = world[x, y] - 10;
-                    if (decoIndex >= 0 && decoIndex < decorates.Count)
-                    {
-                        GameObject spawnedDeco = CreateObjectsOnCoord(decorates[decoIndex].objSpawn, logicX, logicY, parentForDecorates);
-                        if (spawnedDeco.TryGetComponent<SpriteRenderer>(out var sr))
-                        {
-                            sr.sortingOrder = -logicY;
-                        }
-                    }
-                }
             }
         }
 
         Check(offsetX, offsetY);
+    }
+
+    int PlaceRooms(int offsetX, int offsetY, int targetRoomCount)
+    {
+        int mapCenterX = worldX / 2;
+        int mapCenterY = worldY / 2;
+
+        float radius = worldX / 3f;
+        float angleStep = 360f / targetRoomCount;
+
+        Debug.Log($"PlaceRooms START: target={targetRoomCount} radius={radius}");
+
+        for (int i = 0; i < targetRoomCount; i++)
+        {
+            float angle = angleStep * i * Mathf.Deg2Rad;
+
+            int size = UnityEngine.Random.Range(minRoom, maxRoom);
+
+            int posX = mapCenterX + Mathf.RoundToInt(Mathf.Cos(angle) * radius);
+            int posY = mapCenterY + Mathf.RoundToInt(Mathf.Sin(angle) * radius);
+
+            centers[i, 0] = posX;
+            centers[i, 1] = posY;
+            roomSizes[i] = size;
+
+            Debug.Log($"Комната {i}: pos=({posX},{posY}) size={size}");
+
+            DrawRoom(posX + offsetX, posY + offsetY, size);
+
+            if (i != 0)
+            {
+                CreateEnemies(posX, posY,
+                    -size, size,
+                    -size, size);
+
+                SetRoomsDecos(posX + offsetX, posY + offsetY,
+                    -size, size,
+                    -size, size);
+            }
+        }
+
+        Debug.Log($"PlaceRooms DONE: создано {targetRoomCount} комнат");
+
+        return targetRoomCount;
+    }
+
+    void DrawRoom(int centerX, int centerY, int roomSize)
+    {
+        for (int x = -roomSize; x <= roomSize; x++)
+        {
+            for (int y = -roomSize; y <= roomSize; y++)
+            {
+                int tx = centerX + x;
+                int ty = centerY + y;
+                if (tx >= 0 && tx < world.GetLength(0) &&
+                    ty >= 0 && ty < world.GetLength(1))
+                {
+                    world[tx, ty] = 0;
+                }
+            }
+        }
+    }
+
+    void BuildRoomGraph(int roomCount)
+    {
+        roomGraph = new List<int>[roomCount];
+        for (int i = 0; i < roomCount; i++)
+            roomGraph[i] = new List<int>();
+
+        
+        for (int i = 0; i < roomCount - 1; i++)
+        {
+            roomGraph[i].Add(i + 1);
+            roomGraph[i + 1].Add(i);
+        }
+
+      
+    }
+
+
+
+
+    void DigTunnelsByGraph(int offsetX, int offsetY, int roomCount)
+    {
+        HashSet<string> dugPairs = new HashSet<string>();
+
+        for (int i = 0; i < roomCount; i++)
+        {
+            foreach (int j in roomGraph[i])
+            {
+                int a = Mathf.Min(i, j);
+                int b = Mathf.Max(i, j);
+                string key = a + "_" + b;
+
+                if (dugPairs.Contains(key)) continue;
+                dugPairs.Add(key);
+
+                int startX = centers[a, 0] + offsetX;
+                int startY = centers[a, 1] + offsetY;
+                int endX = centers[b, 0] + offsetX;
+                int endY = centers[b, 1] + offsetY;
+
+                DigTunnelX(startX, endX, startY);
+                DigTunnelY(startY, endY, endX);
+            }
+        }
     }
 
     void DigTunnelX(int startX, int endX, int y)
@@ -254,10 +278,9 @@ public class WorldGeneration : MonoBehaviour
         {
             for (int w = -halfWidth; w <= halfWidth; w++)
             {
-                if (y + w >= 0 && y + w < world.GetLength(1))
-                {
-                    world[x, y + w] = 0;
-                }
+                int ty = y + w;
+                if (ty >= 0 && ty < world.GetLength(1))
+                    world[x, ty] = 0;
             }
         }
     }
@@ -272,10 +295,9 @@ public class WorldGeneration : MonoBehaviour
         {
             for (int w = -halfWidth; w <= halfWidth; w++)
             {
-                if (x + w >= 0 && x + w < world.GetLength(0))
-                {
-                    world[x + w, y] = 0;
-                }
+                int tx = x + w;
+                if (tx >= 0 && tx < world.GetLength(0))
+                    world[tx, y] = 0;
             }
         }
     }
@@ -310,22 +332,19 @@ public class WorldGeneration : MonoBehaviour
                     {
                         int tx = randomX + z;
                         int ty = randomY + w;
-                        if (tx < 0 || tx >= world.GetLength(0) || ty < 0 || ty >= world.GetLength(1) || world[tx, ty] != 0)
-                        {
+                        if (tx < 0 || tx >= world.GetLength(0) ||
+                            ty < 0 || ty >= world.GetLength(1) ||
+                            world[tx, ty] != 0)
                             canPlace = false;
-                        }
                     }
                 }
 
                 if (canPlace)
                 {
                     for (int z = 0; z < rad; z++)
-                    {
                         for (int w = 0; w < rad; w++)
-                        {
                             world[randomX + z, randomY + w] = 10 + i;
-                        }
-                    }
+
                     countOfDeco--;
                 }
             }
@@ -344,8 +363,8 @@ public class WorldGeneration : MonoBehaviour
 
             int maxPossible = Mathf.CeilToInt(runtimeEnemies[u].countSpawn / 5f);
             int count = UnityEngine.Random.Range(1, Mathf.Max(2, maxPossible));
-
-            if (runtimeEnemies[u].countSpawn - count < 0) count = runtimeEnemies[u].countSpawn;
+            if (runtimeEnemies[u].countSpawn - count < 0)
+                count = runtimeEnemies[u].countSpawn;
 
             if (count > 0)
             {
@@ -364,8 +383,8 @@ public class WorldGeneration : MonoBehaviour
 
                 int maxPossible = Mathf.CeilToInt(runtimeEnemies[u].countSpawn / 5f);
                 int count = UnityEngine.Random.Range(1, Mathf.Max(2, maxPossible));
-
-                if (runtimeEnemies[u].countSpawn - count < 0) count = runtimeEnemies[u].countSpawn;
+                if (runtimeEnemies[u].countSpawn - count < 0)
+                    count = runtimeEnemies[u].countSpawn;
 
                 if (count > 0)
                 {
@@ -378,7 +397,8 @@ public class WorldGeneration : MonoBehaviour
         }
     }
 
-    void SpawnEnemyGroup(int enemyIndex, int count, int centerX, int centerY, int xMin, int xMax, int yMin, int yMax)
+    void SpawnEnemyGroup(int enemyIndex, int count, int centerX, int centerY,
+                          int xMin, int xMax, int yMin, int yMax)
     {
         int rangeXMin = xMin + 2;
         int rangeXMax = xMax - 1;
@@ -396,7 +416,8 @@ public class WorldGeneration : MonoBehaviour
             float jitterX = UnityEngine.Random.Range(-0.15f, 0.15f);
             float jitterY = UnityEngine.Random.Range(-0.15f, 0.15f);
 
-            CreateObjectsOnCoord(runtimeEnemies[enemyIndex].enemyPrefab, gridX + jitterX, gridY + jitterY, parentForEnemy);
+            CreateObjectsOnCoord(runtimeEnemies[enemyIndex].enemyPrefab,
+                gridX + jitterX, gridY + jitterY, parentForEnemy);
         }
     }
 
@@ -405,15 +426,13 @@ public class WorldGeneration : MonoBehaviour
         tilemapWallRL.ClearAllTiles();
         tilemapWallUD.ClearAllTiles();
 
-        Vector3Int checkPos = Vector3Int.zero;
-
         for (int x = 1; x < world.GetLength(0) - 1; x++)
         {
             for (int y = 1; y < world.GetLength(1) - 1; y++)
             {
                 int logicX = x - offsetX;
                 int logicY = y - offsetY;
-                checkPos.x = logicX; checkPos.y = logicY;
+                Vector3Int checkPos = new Vector3Int(logicX, logicY, 0);
 
                 if (world[x, y] != -1)
                 {
